03c6e924d1ef078debc31dad19e1e4f4d56e076d
Trinidad Pardo <trinipardo@hotmail.com>
2021-06-01 15:09:48 -0300

    stilo.js

--- src/Componentes/Tarjeta.js
+++ src/Componentes/Tarjeta.js
@@ -17,8 +17,8 @@ export class Tarjeta extends Component {
         <View style={styles.tarjeta}>
         
         <Image style={styles.imagen} source={{uri: item.picture.thumbnail}}/>
-        <Text style= {styles.text}> {item.name.first}</Text>
-        <Text style= {styles.text}> {item.name.last}</Text>
+        <Text style= {styles.textTarjeta}> {item.name.first}</Text>
+        <Text style= {styles.textTarjeta}> {item.name.last}</Text>
         
         </View>
     </TouchableOpacity>
@@ -32,4 +32,42 @@ separator=() => {
     )
 }
 }
+
+const styles= StyleSheet.create({
+
+    tarjeta: {
+      borderWidth: 3,
+      borderRadius: 10,
+      borderColor: '#00b8a9',
+      width: 350,
+      height: 600,
+      padding: 20,
+      marginTop: 10,
+      marginBottom: 10,
+      backgroundColor: '#f8f3d4', 
+  
+    },
+  
+   
+     
+     textTarjeta:{
+       fontSize: 15,
+       marginTop: 10,
+       marginBottom: 10,
+      },
+  
+      imagen:{
+        width: 150, 
+        height: 150, 
+        borderRadius: 10,
+       marginLeft: 75,
+       borderColor: '#00b8a9',
+       borderWidth: 3,
+      },
+  
+      separator: {
+        borderBottonColor: 'black',
+        borderBottonWidth: 1,
+      }
+  })
 
