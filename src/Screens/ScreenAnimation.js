3bb72caa0d9844c93c72024f4644ae80646c7327
mjaichenco <56735038+mjaichenco@users.noreply.github.com>
2021-06-16 09:24:27 -0300

    screen animation comentado

---
+++ src/Screens/ScreenAnimation.js
@@ -0,0 +1,9 @@
+//import react, { component } from 'react';
+//import {
+    //View,
+    //Text,
+    //TouchableOpacity,
+  //  Image
+//}from 'react-native';
+
+//export class ScreenAnimation extends component;
